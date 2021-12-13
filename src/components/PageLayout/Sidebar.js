1f54bbcc38a412a167ac232d3acf7a800c229efc
= <=>
2021-12-13 19:43:59 +0600

    finished and booking list and remove

--- src/components/PageLayout/Sidebar.js
+++ src/components/PageLayout/Sidebar.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
+
 const Sidebar = () => {
+  const readingList = useSelector((state) => state.books.readingList )
+  const finished = useSelector((state) => state.books.finishedList)
   return (
     <div className='col-md-3'>
       <ul className='list-group sticky-top  py-2'>
@@ -9,10 +13,10 @@ const Sidebar = () => {
           Discover
         </NavLink>
         <NavLink as='li' className='list-group-item' to='/reading'>
-          Reading List <span className='badge badge-sm bg-primary'>0</span>
+          Reading List <span className='badge badge-sm bg-primary'>{readingList.length}</span>
         </NavLink>
         <NavLink as='li' className='list-group-item' to='/finish'>
-          Finished Books <span className='badge badge-sm bg-primary'>0</span>
+          Finished Books <span className='badge badge-sm bg-primary'>{finished.length}</span>
         </NavLink>
       </ul>
     </div>
